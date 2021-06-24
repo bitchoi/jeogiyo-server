da929d8b8ce729642c7aa9a040fae1404555cc6a
bitchoi <choibit753@gmail.com>
2021-06-24 11:28:51 +0900

    initial commit

---
+++ src/main/java/com/bitchoi/jeogiyoserver/JeogiyoServerApplication.java
@@ -0,0 +1,12 @@
+package com.bitchoi.jeogiyoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeogiyoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeogiyoServerApplication.class, args);
+    }
+}
