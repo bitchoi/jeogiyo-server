4ac32728c9c05fb384fdd691a68480ec14f2cc87
bitchoi <choibit753@gmail.com>
2021-06-29 00:23:38 +0900

    Fix : securityConfig

--- src/main/java/com/bitchoi/jeogiyoserver/config/SecurityConfig.java
+++ src/main/java/com/bitchoi/jeogiyoserver/config/SecurityConfig.java
@@ -18,40 +18,60 @@ import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
 import org.springframework.security.web.util.matcher.OrRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthenticationProvider authenticationProvider;
 
-    private final JwtUtils jwtUtils;
+    // @formatter:off
+    public static final String[] permitAll = {
+             "/swagger-ui.html"
+            , "/auth/**"
+            , "/registrations/**"
+            , "/error"
+    };
+    public static final String[] API_DOCS_PATHS = {
+            "/v2/api-docs"
+            , "/swagger-resources"
+            , "/swagger-resources/**"
+            , "/swagger-ui.html"
+            , "/webjars/**"
+            , "/csrf"
+    };
+    // @formatter:on
+    private final AuthenticationProvider provider;
 
     @Override
-    protected void configure(final AuthenticationManagerBuilder auth){
-        auth.authenticationProvider(authenticationProvider);
+    protected void configure(final AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(provider);
     }
 
     @Bean
-    AuthenticationFilter authenticationFilter() throws  Exception {
-        RequestMatcher rm = new NegatedRequestMatcher(new OrRequestMatcher(new AntPathRequestMatcher("/home")));
-        final var filter = new AuthenticationFilter(rm, jwtUtils);
+    AuthenticationFilter authenticationFilter() throws Exception {
+        RequestMatcher rm =
+                new NegatedRequestMatcher(new OrRequestMatcher(Arrays.stream(permitAll).map(AntPathRequestMatcher::new).collect(Collectors.toList())));
+        final var filter = new AuthenticationFilter(rm);
         filter.setAuthenticationManager(authenticationManager());
         return filter;
     }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .httpBasic().disable()
-                .csrf().disable()
-                .authenticationProvider(authenticationProvider)
+        http.csrf().disable().httpBasic().disable()
+                .authenticationProvider(provider)
                 .addFilterAfter(authenticationFilter(), AnonymousAuthenticationFilter.class)
                 .authorizeRequests()
-                .antMatchers().permitAll()
+                .antMatchers(permitAll).permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
+        http.headers().frameOptions().disable();
     }
-
 }
